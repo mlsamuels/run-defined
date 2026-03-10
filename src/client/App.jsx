c1425bb60ace6254910b57dc5675a6df16253ac1
Matthew Samuels <mlsamuels@wpi.edu>
2026-03-09 22:38:33 -0400

    Setup simple frontend and post for testing backend.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import {useState} from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [text, setText] = useState("");
+    const [resultText, setResultText] = useState("");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+    const buttonPress = async () => {
+        try {
+            const response = await fetch('/testsubmit', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    "code": text
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+
+            setResultText(result["run-result"])
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    return (
+        <div className="App">
+            <h1>RunDefined</h1>
+            <div className="card">
+                <div>
+                    <textarea cols={50} rows={30} onChange={(e) => {
+                        setText(e.target.value)
+                    }}/>
+                </div>
+
+                <button onClick={buttonPress}>
+                    Submit
+                </button>
+                <div>
+                    {resultText}
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/server/main.js
+++ src/server/main.js
@@ -3,9 +3,14 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
+app.use(express.json());
+
+app.post("/testsubmit", async (req, res) => {
+  res.ok=true;
+  const code = req.body.code;
+  res.send({"run-result":code+" is what I received!"});
 });
+
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
