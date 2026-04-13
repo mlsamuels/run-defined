3a204342a5e59a33d34b59e60e2071db01fb6bd1
Matthew Samuels <mlsamuels@wpi.edu>
2026-04-13 13:29:03 -0400

    Fixed container logs for real this time

--- src/server/main.js
+++ src/server/main.js
@@ -4,10 +4,6 @@ import fs from "fs";
 import Docker from "dockerode";
 const docker = new Docker();
 
-import PassThrough  from 'stream';
-
-
-
 const app = express();
 
 app.use(express.json());
@@ -33,52 +29,59 @@ ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
 
-
 async function startContainer() {
   try {
     // Create container
     const container = await docker.createContainer({
       Image: 'python:3.12-slim',
-      Cmd: ['python', '/app/script.py'],
+      Cmd: ['python','-u' ,'/app/script.py'],
       HostConfig: {
         Binds: [`${process.cwd()}/python_scripts/script.py:/app/script.py`],
-        AutoRemove: true,
+        AutoRemove: false,
         Tty: true
       }
     });
 
-    //Setup stream
-    const stream = await container.attach({
-      stream: true,
-      stdout: true,
-      stderr: true,
-      log: true
-    });
-
-    let output=""
-
-    //Create stream callback function, define how chunks are added to outpu
-    stream.on('data', (chunk) => {
-      // [0] = stream type (1=stdout, 2=stderr)
-      // [1-3] = unused
-      // [4-7] = payload length
-      const type = chunk[0];
-      const length = chunk.readUInt32BE(4);
-      const content = chunk.slice(8, 8 + length).toString();
-
-      if (type === 1) {
-        output += content; // stdout
-      } else if (type === 2) {
-        output += content; // stderr
-      }
-    });
-
     await container.start();
-    await container.wait();
+    const result=await container.wait();
 
-    return output;
+    console.log(result["StatusCode"])
+    const logs = await container.logs({
+      stdout: true,
+      stderr: true
+    });
+    await container.remove();
+
+    console.log(decodeDockerLogs(logs))
+    return decodeDockerLogs(logs);
 
   } catch (err) {
     console.error('Failed to start container:', err);
   }
+}
+function decodeDockerLogs(buffer) {
+  let offset = 0;
+  let output = '';
+
+  while (offset < buffer.length) {
+    // Docker header format:
+    // byte 0 = stream type (1=stdout, 2=stderr)
+    // bytes 1-3 = unused
+    // bytes 4-7 = length
+    const length = buffer.readUInt32BE(offset + 4);
+
+    // extract payload safely
+    const start = offset + 8;
+    const end = start + length;
+
+    if (end > buffer.length) break; // safety guard
+
+    const chunk = buffer.slice(start, end);
+
+    output += chunk.toString();
+
+    offset += 8 + length;
+  }
+
+  return output;
 }
