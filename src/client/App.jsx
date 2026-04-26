5c56521ca61f500442e7e93712c1d5402b720840
Matthew Samuels <mlsamuels@wpi.edu>
2026-04-26 17:52:20 -0400

    Added Game playing, elo system, user ranking

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -8,11 +8,11 @@ function App() {
     const [outText, setOutText] = useState("");
     const [errText, setErrText] = useState("");
 
-    const buttonPress = async () => {
+    const testPress = async () => {
         setOutText("");
         setErrText("");
         try {
-            const response = await fetch('/testsubmit', {
+            const response = await fetch('/testfunction', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
@@ -33,6 +33,34 @@ function App() {
 
     }
 
+    const submitPress = async () => {
+        setOutText("");
+        setErrText("");
+        try {
+            const response = await fetch('/submitfunction', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    "code": localStorage.getItem("code"),
+                    "name": localStorage.getItem("name")
+                }),
+            });
+
+            if (!response.ok) {
+                console.log((await response.json())["error"])
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+
+            setOutText(JSON.parse(result["stdout"]))
+            setErrText(JSON.parse(result["stderr"]))
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+
     //Callback function for code editor
     const onChange = (newText) => {
         localStorage.setItem("code", newText);
@@ -42,11 +70,23 @@ function App() {
         <div className="App">
             <h1>RunDefined</h1>
             <div className="card">
-                <div id="editor-div">
-                    {editorComponent(localStorage.getItem("code"), onChange)}
+                <div>
+                    <h2>Name:</h2>
+                    <div>
+                        <input onChange={(event)=>{localStorage.setItem("name", event.target.value)}} />
+                    </div>
+                </div>
+                <div>
+                    <h2>Code:</h2>
+                    <div id="editor-div">
+                        {editorComponent(localStorage.getItem("code"), onChange)}
+                    </div>
                 </div>
                 <br/>
-                <button className="button" onClick={buttonPress}>
+                <button className="button" onClick={testPress}>
+                    Test
+                </button>
+                <button className="button" onClick={submitPress}>
                     Submit
                 </button>
                 {runResultComponent(outText, "stdout", false)}
--- src/server/main.js
+++ src/server/main.js
@@ -14,12 +14,12 @@ const app = express();
 await mongoose.connect(process.env.MONGODB_URI, {});
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
-db.once("open", (open) => console.log("Connected to database"));
 const gameZeroSubmissions = db.collection("gameZeroSubmissions");
+
 
 app.use(express.json());
 
-app.post("/testsubmit", async (req, res) => {
+app.post("/testfunction", async (req, res) => {
   res.ok=true;
   const code = req.body.code;
 
@@ -31,15 +31,89 @@ app.post("/testsubmit", async (req, res) => {
   }
 
   //Run python file and get output
-  const output=await startContainer("python", "pythonContainer", 8000,9000)
+  const output=await startContainer()
   res.send({"stdout":JSON.stringify(output[0]), "stderr":JSON.stringify(output[1])});
 });
 
+app.post("/submitfunction", async (req, res) => {
+
+  const code = req.body.code;
+  const name = req.body.name;
+
+  //check if name in use
+  const existing = await gameZeroSubmissions.findOne({name: name})
+  if(existing){
+    req.ok =false;
+    res.send({"error": "Name in use"});
+  }
+
+  //TODO Make sure code works before adding to database
+
+  //add to database
+  const insert_result = await gameZeroSubmissions.insertOne({name: name, code: code, elo: 1500})
+  const id = insert_result.insertedId;
+
+  await playGames(id, 5)
+
+  res.ok=true;
+
+  res.send({});
+});
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
 
+async function playGames(id, count){
+
+  const opponents = await gameZeroSubmissions.find({ _id: { $ne: id } }).toArray()
+  const real_count =opponents.length
+  count = Math.min(count,real_count)
+
+  const final_opponents = opponents.sort(() => Math.random() - 0.5).slice(0,count)
+  for (let i = 0; i < final_opponents.length; i++) {
+    const id2 = final_opponents[i]._id
+    await playGame(id,id2)
+  }
+}
+
+async function playGame(id1, id2){
+  const p1=await gameZeroSubmissions.findOne({_id: id1})
+  const p2=await gameZeroSubmissions.findOne({_id: id2})
+
+  //TODO encapsulate this code
+  //write to script.py player 1
+  try {
+    fs.writeFileSync("python_scripts/script.py", p1.code); // Specify encoding
+  } catch (err) {
+    console.error('Error writing file:', err);
+  }
+
+  //Run python file and get output
+  const output1=await startContainer()
+  const p1_score = parseInt(output1[0].split(/\r?\n/).at(-2));
+
+  //write to script.py player 2
+  try {
+    fs.writeFileSync("python_scripts/script.py", p2.code); // Specify encoding
+  } catch (err) {
+    console.error('Error writing file:', err);
+  }
+
+  //Run python file and get output
+  const output2=await startContainer()
+  const p2_score = parseInt(output2[0].split(/\r?\n/).at(-2));
+
+  //compute elo change
+  const result = p1_score>p2_score?0:1;
+  const eloChange=eloUpdate(p1.elo,p2.elo,result);
+
+  //update elos
+  await gameZeroSubmissions.updateOne({_id:id1},{$set:{elo:p1.elo+eloChange*(1.0-result)-eloChange*result}})
+  await gameZeroSubmissions.updateOne({_id:id2},{$set:{elo:p2.elo+eloChange*result-eloChange*(1.0-result)}})
+}
+
+//Run python code and get results
 async function startContainer() {
   try {
     // Create container
@@ -98,4 +172,11 @@ async function startContainer() {
   } catch (err) {
     console.error('Failed to start container:', err);
   }
+}
+
+function eloUpdate(elo0, elo1, winner){
+  const p0=(1.0/(1.0+10**((elo1-elo0)/400)))
+  const p1=1.0-p0
+  const K=30
+  return winner*K*p0 + (1.0-winner)*K*p1
 }
