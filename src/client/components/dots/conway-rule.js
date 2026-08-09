ef12eccfcf9e3e1cce871d65dcf3e39b1185ba8e
Matthew Samuels <mlsamuels@wpi.edu>
2026-08-09 14:51:03 -0400

    Fixed conway bug

--- src/client/components/dots/conway-rule.js
+++ src/client/components/dots/conway-rule.js
@@ -9,23 +9,24 @@ export class conwayRule{
         [[2,3,4],[3],0.2, "Ant Colony"],
         [[2,3],[3,6],0.2, "High Life"],
         [[3,4,6,7,8],[3,6,7,8],0.5, "Day and Night"],
-        [[0,1,2,3,4,5,6,7,8],[3],0.04, "Life without Death"],
+        [[0,1,2,3,4,5,6,7,8],[3],0.05, "Life without Death"],
         [[4,5,6,7,8],[3],0.5, "Coral"],
         [[0,1,2,3,4,5,6,7,8],[1],0.002, "H-Trees"],
         [[1,2],[3],0.2, "Flock"],
-        [[4,5,6,7],[3,4,5],0.4, "Assimilation"],
-        [[2,3,4,5,6,7],[3,5],0.2, "Land Rish"],
+        [[4,5,6,7],[3,4,5],0.2, "Assimilation"],
+        [[2,3,4,5,6,7],[3,5],0.2, "Land Rush"],
         [[1,3,5,6,7,8],[3,6,7,8],0.3, "Castles"],
         [[2,3,5,6,7,8],[3,6,7,8],0.2, "Stains"],
         [[2,3,4,5,6,7,8],[5,6,7,8],0.4, "Smoothers"],
         [[8,7,6,5,4],[8,7,6,5],0.6, "Vote"],
+        [[2,4,5],[3,6,8],0.2, "Move"],
         [[0,1,2,3,4,5,6,7].sort(()=>Math.random()-0.5).slice(0,1+Math.random()*5),[1,2,3,4,5,6,7].sort(()=>Math.random()-0.5).slice(0,1+Math.random()*5),Math.random()*0.5+0.05, "Random"]
     ]
+
     static u = conwayRule.universes[Math.trunc(Math.random()*conwayRule.universes.length)];
     static s= conwayRule.u[0]
     static b=conwayRule.u[1]
     static r=conwayRule.u[2]
-
 
 
     static defaultArray(height, width){
@@ -34,7 +35,7 @@ export class conwayRule{
             Array(width).fill(null));
         for (let i = 0; i<newArray.length; i++) {
             for (let j = 0; j < newArray[0].length; j++) {
-                newArray[i][j]=Math.random()>this.r?"#FFFFFF":"#000000";
+                newArray[i][j]=Math.random()>this.r?"#ffffff":"#000000";
             }
         }
         return newArray;
